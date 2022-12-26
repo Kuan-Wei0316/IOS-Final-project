c1228401f1ad35127da72da79e414a35543eb9b3
fuck <fuck@ass.hole>
2022-12-26 15:15:41 +0800

    Initial Commit

---
+++ IOS Final project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS Final project
+//
+//  Created by User11 on 2022/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS Final project/IOS_Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Final_projectApp.swift
+//  IOS Final project
+//
+//  Created by User11 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
